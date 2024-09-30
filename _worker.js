03182073bb2b51543f8cfd7a3a66da48eb4905b5
Amir Dark <amdark77@gmail.com>
2024-09-30 15:31:30 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -14,7 +14,7 @@ const proxyIPs= ['bpb.yousef.isegaro.com'];
 const defaultHttpPorts = ['80', '8080', '2052', '2082', '2086', '2095', '8880'];
 const defaultHttpsPorts = ['443', '8443', '2053', '2083', '2087', '2096'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
-let dohURL = 'https://cloudflare-dns.com/dns-query';
+let dohURL = 'https://g9joq9xog0.cloudflare-gateway.com/dns-query';
 let trojanPassword = `bpb-trojan`;
 // https://emn178.github.io/online-tools/sha224.html
 // https://www.atatus.com/tools/sha224-to-hash
